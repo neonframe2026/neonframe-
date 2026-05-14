8ff3fa102f6d0cebff63d24eb858b1e0160df0a8
neonframe2026 <info@neonframe.de>
2026-05-14 03:47:24 +0200

    Update page.js

--- app/angebot/[id]/page.js
+++ app/angebot/[id]/page.js
@@ -228,7 +228,7 @@ export default async function AngebotPage({ params }) {
         .img-tt-box img { display:block; border-radius:8px; }
         .img-tt-box.square img { width:148px; height:148px; object-fit:cover; }
 .img-tt-box.wide img {
-  width: 500px;
+  width: 550px;
   max-width: 92vw;
   height: auto;
   object-fit: contain;
